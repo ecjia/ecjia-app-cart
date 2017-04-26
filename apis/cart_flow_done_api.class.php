957bd0995b70e0f7dc4e1b9f9c584a3e4ff93b4b
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-04-26 16:34:55 +0800

    消息通知发送更新范围，和推送互相独立

--- apis/cart_flow_done_api.class.php
+++ apis/cart_flow_done_api.class.php
@@ -542,33 +542,33 @@ class cart_flow_done_api extends Component_Event_Api {
 		$staff_user = RC_DB::table('staff_user')->where('store_id', $order['store_id'])->where('parent_id', 0)->first();
 		if (!empty($staff_user)) {
 			$devic_info = RC_Api::api('mobile', 'device_info', array('user_type' => 'merchant', 'user_id' => $staff_user['user_id']));
+			/* 通知记录*/
+			$orm_staff_user_db = RC_Model::model('express/orm_staff_user_model');
+			$staff_user_ob = $orm_staff_user_db->find($staff_user['user_id']);
+			
+			$order_data = array(
+			    'title'	=> '客户下单',
+			    'body'	=> '您有一笔新订单，订单号为：'.$order['order_sn'],
+			    'data'	=> array(
+			        'order_id'		         => $order['order_id'],
+			        'order_sn'		         => $order['order_sn'],
+			        'order_amount'	         => $order['order_amount'],
+			        'formatted_order_amount' => price_format($order['order_amount']),
+			        'consignee'		         => $order['consignee'],
+			        'mobile'		         => $order['mobile'],
+			        'address'		         => $order['address'],
+			        'order_time'	         => RC_Time::local_date(ecjia::config('time_format'), $order['add_time']),
+			    ),
+			);
+			
+			$push_order_placed = new OrderPlaced($order_data);
+			RC_Notification::send($staff_user_ob, $push_order_placed);
 		}
 		
 		if (!is_ecjia_error($devic_info) && !empty($devic_info)) {
 			$push_event = RC_Model::model('push/push_event_viewmodel')->where(array('event_code' => 'order_placed', 'is_open' => 1, 'status' => 1, 'mm.app_id is not null', 'mt.template_id is not null', 'device_code' => $devic_info['device_code'], 'device_client' => $devic_info['device_client']))->find();
 			
 			if (!empty($push_event)) {
-				/* 通知记录*/
-				$orm_staff_user_db = RC_Model::model('express/orm_staff_user_model');
-				$staff_user_ob = $orm_staff_user_db->find($staff_user['user_id']);
-				
-				$order_data = array(
-					'title'	=> '客户下单',
-					'body'	=> '您有一笔新订单，订单号为：'.$order['order_sn'],
-					'data'	=> array(
-						'order_id'		         => $order['order_id'],
-						'order_sn'		         => $order['order_sn'],
-						'order_amount'	         => $order['order_amount'],
-						'formatted_order_amount' => price_format($order['order_amount']),
-						'consignee'		         => $order['consignee'],
-						'mobile'		         => $order['mobile'],
-						'address'		         => $order['address'],
-						'order_time'	         => RC_Time::local_date(ecjia::config('time_format'), $order['add_time']),
-					),
-				);
-				
-				$push_order_placed = new OrderPlaced($order_data);
-				RC_Notification::send($staff_user_ob, $push_order_placed);
 				
 				RC_Loader::load_app_class('push_send', 'push', false);
 				ecjia_admin::$controller->assign('order', $order);
