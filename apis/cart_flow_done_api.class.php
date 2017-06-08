7b8bdc47210ec680a68a72bfcc17a8ebd473842c
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-06-08 11:51:30 +0800

    更新短信发送方法

--- apis/cart_flow_done_api.class.php
+++ apis/cart_flow_done_api.class.php
@@ -393,23 +393,34 @@ class cart_flow_done_api extends Component_Event_Api {
 			$staff_user = RC_DB::table('staff_user')->where('store_id', $order['store_id'])->where('parent_id', 0)->first();
 			if (ecjia::config('sms_order_placed')== '1' && !empty($staff_user['mobile'])) {
 				//发送短信
-				$tpl_name = 'order_placed_sms';
-				$tpl   = RC_Api::api('sms', 'sms_template', $tpl_name);
-				if (!empty($tpl)) {
-					ecjia_front::$controller->assign('order',	$order);
-					ecjia_front::$controller->assign('consignee', $order['consignee']);
-					ecjia_front::$controller->assign('mobile',	$order['mobile']);
+// 				$tpl_name = 'order_placed_sms';
+// 				$tpl   = RC_Api::api('sms', 'sms_template', $tpl_name);
+// 				if (!empty($tpl)) {
+// 					ecjia_front::$controller->assign('order',	$order);
+// 					ecjia_front::$controller->assign('consignee', $order['consignee']);
+// 					ecjia_front::$controller->assign('mobile',	$order['mobile']);
 					
-					$content = ecjia_front::$controller->fetch_string($tpl['template_content']);
-					$msg = $order['pay_status'] == PS_UNPAYED ? $content : $content.__('已付款');
+// 					$content = ecjia_front::$controller->fetch_string($tpl['template_content']);
+// 					$msg = $order['pay_status'] == PS_UNPAYED ? $content : $content.__('已付款');
 					
-					$params = array(
-							'mobile' 		=> $staff_user['mobile'],
-							'msg'			=> $msg,
-							'template_id' 	=> $tpl['template_id'],
-					);
-					$response = RC_Api::api('sms', 'sms_send', $params);
-				}
+// 					$params = array(
+// 							'mobile' 		=> $staff_user['mobile'],
+// 							'msg'			=> $msg,
+// 							'template_id' 	=> $tpl['template_id'],
+// 					);
+// 					$response = RC_Api::api('sms', 'sms_send', $params);
+// 				}
+				
+				$options = array(
+					'mobile' => $staff_user['mobile'],
+					'event'	 => 'sms_order_placed',
+					'value'  =>array(
+						'consignee' 	=> $order['consignee'],
+						'order_mobile'  => $order['mobile'],
+						'service_phone' => ecjia::config('service_phone'),
+					),
+				);
+				$response = RC_Api::api('sms', 'send_event_sms', $options);
 			}
 		}
 		/* 如果订单金额为0 处理虚拟卡 */
