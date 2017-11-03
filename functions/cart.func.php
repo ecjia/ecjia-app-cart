297a1992f602eda396e7bcb1e179794fca25bcb7
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-11-03 10:53:01 +0800

    admin/flow/done订单积分金额修改

--- functions/cart.func.php
+++ functions/cart.func.php
@@ -670,9 +670,14 @@ function addto_cart($goods_id, $num = 1, $spec = array(), $parent = 0,$warehouse
  * @access  private
  * @return  integral
  */
-function flow_available_points($cart_id = array()) {
+function flow_available_points($cart_id = array(), $device) {
+	if ($device['code'] == '8001') {
+		$rec_type = CART_CASHDESK_GOODS;
+	} else{
+		$rec_type = CART_GENERAL_GOODS;
+	}
 	$db_view = RC_Loader::load_app_model('cart_goods_viewmodel', 'cart');
-	$cart_where = array('c.user_id' => $_SESSION['user_id'], 'c.is_gift' => 0 , 'g.integral' => array('gt' => '0') , 'c.rec_type' => CART_GENERAL_GOODS);
+	$cart_where = array('c.user_id' => $_SESSION['user_id'], 'c.is_gift' => 0 , 'g.integral' => array('gt' => '0') , 'c.rec_type' => $rec_type);
 	if (!empty($cart_id)) {
 		$cart_where = array_merge($cart_where, array('rec_id' => $cart_id));
 	}
