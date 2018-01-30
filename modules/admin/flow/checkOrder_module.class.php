6a478011042477e956ba259e7ab23cb3a43e179a
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-01-30 10:19:13 +0800

    收银台购物流接口修改

--- modules/admin/flow/checkOrder_module.class.php
+++ modules/admin/flow/checkOrder_module.class.php
@@ -12,7 +12,9 @@ class checkOrder_module extends api_admin implements api_interface {
         if ($_SESSION['admin_id'] <= 0 && $_SESSION['staff_id'] <= 0) {
             return new ecjia_error(100, 'Invalid session');
         }
-        define('SESS_ID', RC_Session::session()->getSessionKey());
+        //define('SESS_ID', RC_Session::session()->getSessionKey());
+        define('SESS_ID', RC_Session::session_id());
+        
         
         RC_Loader::load_app_class('cart', 'cart', false);
         
