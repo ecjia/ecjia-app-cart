897a44cb83d3c65d8d75aacf046c709aa3f1f0f3
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-02 11:15:40 +0800

    修改查询

--- apis/cart_flow_done_api.class.php
+++ apis/cart_flow_done_api.class.php
@@ -153,7 +153,7 @@ class cart_flow_done_api extends Component_Event_Api {
 				if (!empty($cart_goods)) {
 					$goods_id = $cart_goods['0']['goods_id'];
 				}
-				$extension_id = RC_DB::table('goods_activity')->where('store_id', $cart_goods['0']['store_id'])->where('goods_id', $goods_id)->where('act_type', GAT_GROUP_BUY)->pluck('act_id');
+				$extension_id = RC_DB::table('goods_activity')->where('store_id', $cart_goods['0']['store_id'])->where('goods_id', $goods_id)->where('act_type', GAT_GROUP_BUY)->orderBy('act_id', 'desc')->pluck('act_id');
 				$order['extension_id'] = empty($extension_id) ? 0 : intval($extension_id);
 				
 				RC_Logger::getLogger('error')->info('test333');
