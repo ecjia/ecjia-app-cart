af5273e97d2640e690da53cbbbf5c3f5a674643d
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-10-10 18:25:10 +0800

    结算后清空购物车，挂单和非挂单数据区分清除

--- classes/cart_cashdesk.class.php
+++ classes/cart_cashdesk.class.php
@@ -1434,11 +1434,16 @@ class cart_cashdesk {
 	 * 清空购物车
 	 * @param   int	 $type   类型：默认普通商品
 	 */
-	public static function clear_cart($type = CART_GENERAL_GOODS, $cart_id = array()) {
+	public static function clear_cart($type = CART_GENERAL_GOODS, $cart_id = array(), $pendorder_id= 0) {
 		$db_cart = RC_Loader::load_app_model('cart_model', 'cart');
 		$cart_w = array('rec_type' => $type);
 		if (!empty($cart_id)) {
 			$cart_w = array_merge($cart_w, array('rec_id' => $cart_id));
+		}
+		if (!empty($pendorder_id)) {
+			$cart_w = array_merge($cart_w, array('pendorder_id' => $pendorder_id));
+		} else {
+			$cart_w = array_merge($cart_w, array('pendorder_id' => 0));
 		}
 		if ($_SESSION['user_id']) {
 			$cart_w = array_merge($cart_w, array('user_id' => $_SESSION['user_id']));
