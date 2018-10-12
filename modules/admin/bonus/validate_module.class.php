3079cc8f3d201db4a9537444826535556b684eb5
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-10-12 14:39:21 +0800

    自动拒单修改

--- modules/admin/bonus/validate_module.class.php
+++ modules/admin/bonus/validate_module.class.php
@@ -69,7 +69,7 @@ class admin_bonus_validate_module extends api_admin implements api_interface
 		$now = RC_Time::gmtime();
 		
 		/* 取得购物类型 */
-		$flow_type  = isset($_SESSION['flow_type']) ? intval($_SESSION['flow_type']) : CART_GENERAL_GOODS;
+// 		$flow_type  = isset($_SESSION['flow_type']) ? intval($_SESSION['flow_type']) : CART_GENERAL_GOODS;
 		
         if (empty($bonus)) {
 			return new ecjia_error('bonus_error', '红包信息有误！');
@@ -77,14 +77,14 @@ class admin_bonus_validate_module extends api_admin implements api_interface
 		if ($bonus['order_id'] > 0) {
 		    return new ecjia_error('bonus_error', '红包已使用！');
 		}
-		if ($bonus['min_goods_amount'] > cart_amount(true, $flow_type)) {
-		    return new ecjia_error('bonus_error', '红包使用最小金额为'.$bonus['min_goods_amount'].'！');
-		}
+// 		if ($bonus['min_goods_amount'] > cart_amount(true, $flow_type)) {
+// 		    return new ecjia_error('bonus_error', '红包使用最小金额为'.$bonus['min_goods_amount'].'！');
+// 		}
 		if ($now < $bonus['use_start_date'] ||  $now > $bonus['use_end_date']) {
 		    return new ecjia_error('bonus_error', '红包不在有效期！');
 		}
 		
-		if (isset($_SESSION['user_id']) && $bonus['user_id'] > 0 && $_SESSION['user_id'] != $bonus['user_id']) {
+		if ($bonus['user_id'] > 0) {
 		    return new ecjia_error('bonus_error', '红包信息有误！');
 		} else {
 		    return array('bonus' => $bonus['type_money'], 'bonus_formated' => price_format($bonus['type_money']));
