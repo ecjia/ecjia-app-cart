53add30e1f10b4eee0b534c3f1789132ae464973
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-01-05 09:37:51 +0800

    调试配送

--- functions/cart.func.php
+++ functions/cart.func.php
@@ -2014,7 +2014,8 @@ function get_ru_shippng_info($goods_list, $cart_value, $store_id, $region = [], 
     }
     
     $shipping_list = ecjia_shipping::availableUserShippings($region, $store_id);
-    
+    RC_Logger::getlogger('info')->info('shipping-list cart.func.php');
+    RC_Logger::getlogger('info')->info(json_encode($shipping_list));
     if($shipping_list) {
         RC_Loader::load_app_class('cart', 'cart', false);
         $is_has_ship_cac = 0;
@@ -2047,7 +2048,7 @@ function get_ru_shippng_info($goods_list, $cart_value, $store_id, $region = [], 
             $shipping_list[$key]['format_shipping_fee'] = price_format($shipping_fee, false);
         }
     }
-
+    RC_Logger::getlogger('info')->info(json_encode($shipping_list));
     $shipping_list = array_unique($shipping_list);
     
     return $shipping_list;
