6042f48451b646a8b0d3b9943dcbb22fd10dabb5
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-02-27 13:12:03 +0800

    最优惠优惠活动获取

--- classes/cart_bbc.class.php
+++ classes/cart_bbc.class.php
@@ -351,10 +351,33 @@ class cart_bbc {
     		}
     		 
     		//获取最优惠的活动信息
-    		$best_fav_key = array_search(max($cart_discount_temp),$cart_discount_temp);
+    		$best_fav = self::get_best_fav($favourable_group);
     		
-    		return array('store_fav_activity' => $favourable_group[$best_fav_key], 'store_cart_discount' => $cart_discount);
+    		return array('store_fav_activity' => $best_fav, 'store_cart_discount' => $cart_discount);
     	}
+    }
+    
+    /**
+     * 获取最优活动
+     * @param array $favourable_group
+     */
+    public static function get_best_fav($favourable_group = array())
+    {
+    	$best_fav = [];
+    	
+    	if ($favourable_group) {
+    		$favourable_group_new = [];
+    		foreach ($favourable_group as $key => $val) {
+    			$favourable_group_new[$val['activity_id']] =  $val['can_discount'];
+    		}
+    		$max_activity_id = array_search(max($favourable_group_new),$favourable_group_new);
+    		foreach ($favourable_group as $k => $v) {
+    			if ($max_activity_id == $v['activity_id']) {
+    				$best_fav = $v;
+    			}
+    		}
+    	}
+    	return $best_fav;
     }
     
     /**
