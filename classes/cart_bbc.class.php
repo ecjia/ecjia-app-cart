efcbc6ae676ba0528e8008849407d2c76607d9e1
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-02-27 19:15:51 +0800

    去除调试

--- classes/cart_bbc.class.php
+++ classes/cart_bbc.class.php
@@ -962,12 +962,7 @@ class cart_bbc {
     	$i = 0;
     
     	//子订单
-    	RC_Logger::getLogger('error')->info('testaaa');
-    	RC_Logger::getLogger('error')->info($shippings);
-    	
-    	
     	foreach ($shippings as $key => $row) {
-    		RC_Logger::getLogger('error')->info('testbbb');
     		$i +=1;
     		$row = $order;
     		unset($row['order_id']);unset($row['order_sn']);unset($row['shippings']);
@@ -1107,15 +1102,10 @@ class cart_bbc {
     
     		$error_no = 0;
     		$new_order_id_child = 0;
-    		RC_Logger::getLogger('error')->info('testqqq');
-    		RC_Logger::getLogger('error')->info($row);
     		do {
     			try {
-    				RC_Logger::getLogger('error')->info('testccc');
     				$row['order_sn'] = ecjia_order_buy_sn();
     				$new_order_id_child = RC_DB::table('order_info')->insertGetId($row);
-    				RC_Logger::getLogger('error')->info($new_order_id_child);
-    				RC_Logger::getLogger('error')->info('testwww');
     			} catch(Exception $e) {
     				$error = $e->getMessage();
     				if($error) {
