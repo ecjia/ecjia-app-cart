bf6a953e2f4f19ff5da0d9191223a69c5a6db7ed
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-28 15:35:38 +0800

    分离购物车的设置参数

--- classes/SettingComponents/ShoppingFlowSetting.php
+++ classes/SettingComponents/ShoppingFlowSetting.php
@@ -40,6 +40,14 @@ class ShoppingFlowSetting extends ComponentAbstract
             ['code' => 'use_bonus', 'value' => '1', 'options' => ['type' => 'select', 'store_range' => '1,0']],
             ['code' => 'use_surplus', 'value' => '1', 'options' => ['type' => 'select', 'store_range' => '1,0']],
             ['code' => 'use_how_oos', 'value' => '1', 'options' => ['type' => 'select', 'store_range' => '1,0']],
+            ['code' => 'can_invoice', 'value' => '1', 'options' => ['type' => 'select', 'store_range' => '1,0']],
+            ['code' => 'invoice_content', 'value' => "水果蔬菜\r\n肉禽蛋奶\r\n冷热速食\r\n休闲食品", 'options' => ['type' => 'textarea']],
+            ['code' => 'invoice_type', 'value' => 'a:2:{s:4:"type";a:3:{i:0;s:12:"普通发票";i:1;s:15:"增值税发票";i:2;s:0:"";}s:4:"rate";a:3:{i:0;d:0;i:1;d:13;i:2;d:0;}}', 'options' => ['type' => 'manual']],
+            ['code' => 'one_step_buy', 'value' => '0', 'options' => ['type' => 'select', 'store_range' => '1,0']],
+            ['code' => 'min_goods_amount', 'value' => '0', 'options' => ['type' => 'text']],
+            ['code' => 'anonymous_buy', 'value' => '1', 'options' => ['type' => 'select', 'store_range' => '1,0']],
+            ['code' => 'cart_confirm', 'value' => '3', 'options' => ['type' => 'options', 'store_range' => '1,2,3,4']],
+            ['code' => 'stock_dec_time', 'value' => '1', 'options' => ['type' => 'select', 'store_range' => '1,0']],
 
         ];
 
@@ -90,6 +98,78 @@ class ShoppingFlowSetting extends ComponentAbstract
                 ),
             ],
 
+            [
+                'cfg_code' => 'can_invoice',
+                'cfg_name' => __('能否开发票', 'goods'),
+                'cfg_desc' => '',
+                'cfg_range' => array(
+                    '0' => __('不能', 'goods'),
+                    '1' => __('能', 'goods'),
+                ),
+            ],
+
+            [
+                'cfg_code' => 'invoice_content',
+                'cfg_name' => __('发票内容', 'goods'),
+                'cfg_desc' => __('客户要求开发票时可以选择的内容。例如：办公用品。每一行代表一个选项。', 'goods'),
+                'cfg_range' => '',
+            ],
+
+            [
+                'cfg_code' => 'invoice_type',
+                'cfg_name' => __('发票类型及税率', 'goods'),
+                'cfg_desc' => '',
+                'cfg_range' => '',
+            ],
+
+            [
+                'cfg_code' => 'one_step_buy',
+                'cfg_name' => __('是否一步购物', 'goods'),
+                'cfg_desc' => '',
+                'cfg_range' => array(
+                    '0' => __('否', 'goods'),
+                    '1' => __('是', 'goods'),
+                ),
+            ],
+
+            [
+                'cfg_code' => 'min_goods_amount',
+                'cfg_name' => __('最小购物金额', 'goods'),
+                'cfg_desc' => __('达到此购物金额，才能提交订单。', 'goods'),
+                'cfg_range' => '',
+            ],
+
+            [
+                'cfg_code' => 'anonymous_buy',
+                'cfg_name' => __('是否允许未登录用户购物', 'goods'),
+                'cfg_desc' => '',
+                'cfg_range' => array(
+                    '0' => __('不允许', 'goods'),
+                    '1' => __('允许', 'goods'),
+                ),
+            ],
+
+            [
+                'cfg_code' => 'cart_confirm',
+                'cfg_name' => __('购物车确定提示', 'goods'),
+                'cfg_desc' => __('允许您设置用户点击“加入购物车”后是否提示以及随后的动作。', 'goods'),
+                'cfg_range' => array(
+                    '1' => __('提示用户，点击“确定”进购物车', 'goods'),
+                    '2' => __('提示用户，点击“取消”进购物车', 'goods'),
+                    '3' => __('直接进入购物车', 'goods'),
+                    '4' => __('不提示并停留在当前页面', 'goods'),
+                ),
+            ],
+
+            [
+                'cfg_code' => 'stock_dec_time',
+                'cfg_name' => __('减库存的时机', 'goods'),
+                'cfg_desc' => '',
+                'cfg_range' => array(
+                    '0' => __('发货时', 'goods'),
+                    '1' => __('下订单时', 'goods'),
+                ),
+            ],
 
         ];
 
