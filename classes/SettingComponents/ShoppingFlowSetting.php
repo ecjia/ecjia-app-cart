b95635463f3eb7f5c8bf6ae110d771d5778e6978
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-04 19:16:27 +0800

    到家模块描述语言添加

--- classes/SettingComponents/ShoppingFlowSetting.php
+++ classes/SettingComponents/ShoppingFlowSetting.php
@@ -22,7 +22,7 @@ class ShoppingFlowSetting extends ComponentAbstract
 
     public function __construct()
     {
-        $this->name = __('购物流程', 'article');
+        $this->name = __('购物流程', 'cart');
     }
 
 
@@ -53,114 +53,114 @@ class ShoppingFlowSetting extends ComponentAbstract
         $config = [
             [
                 'cfg_code' => 'use_integral',
-                'cfg_name' => __('是否使用积分', 'article'),
-                'cfg_desc' => '',
+                'cfg_name' => __('是否使用积分', 'cart'),
+                'cfg_desc' => __('设置“使用”则表示用户在结算时，可使用积分抵扣订单金额结算', 'cart'),
                 'cfg_range' => array(
-                    '0' => __('不使用', 'article'),
-                    '1' => __('使用', 'article'),
+                    '0' => __('不使用', 'cart'),
+                    '1' => __('使用', 'cart'),
                 ),
             ],
 
             [
                 'cfg_code' => 'use_bonus',
-                'cfg_name' => __('是否使用红包', 'article'),
-                'cfg_desc' => '',
+                'cfg_name' => __('是否使用红包', 'cart'),
+                'cfg_desc' => __('设置“使用”则表示用户在结算时，可使用红包抵扣订单金额结算', 'cart'),
                 'cfg_range' => array(
-                    '0' => __('不使用', 'article'),
-                    '1' => __('使用', 'article'),
+                    '0' => __('不使用', 'cart'),
+                    '1' => __('使用', 'cart'),
                 ),
             ],
 
             [
                 'cfg_code' => 'use_surplus',
-                'cfg_name' => __('是否使用余额', 'article'),
-                'cfg_desc' => '',
+                'cfg_name' => __('是否使用余额', 'cart'),
+                'cfg_desc' => __('设置“使用”则表示用户在结算时，可使用余额抵扣订单金额结算', 'cart'),
                 'cfg_range' => array(
-                    '0' => __('不使用', 'article'),
-                    '1' => __('使用', 'article'),
+                    '0' => __('不使用', 'cart'),
+                    '1' => __('使用', 'cart'),
                 ),
             ],
 
             [
                 'cfg_code' => 'use_how_oos',
-                'cfg_name' => __('是否使用缺货处理', 'article'),
-                'cfg_desc' => __('使用缺货处理时前台订单确认页面允许用户选择缺货时处理方法。', 'article'),
+                'cfg_name' => __('是否使用缺货处理', 'cart'),
+                'cfg_desc' => __('使用缺货处理时前台订单确认页面允许用户选择缺货时处理方法。', 'cart'),
                 'cfg_range' => array(
-                    '0' => __('不使用', 'article'),
-                    '1' => __('使用', 'article'),
+                    '0' => __('不使用', 'cart'),
+                    '1' => __('使用', 'cart'),
                 ),
             ],
 
             [
                 'cfg_code' => 'can_invoice',
-                'cfg_name' => __('能否开发票', 'article'),
-                'cfg_desc' => '',
+                'cfg_name' => __('能否开发票', 'cart'),
+                'cfg_desc' => __('设置“能”则表示用户在结算时，可选择开具发票', 'cart'),
                 'cfg_range' => array(
-                    '0' => __('不能', 'article'),
-                    '1' => __('能', 'article'),
+                    '0' => __('不能', 'cart'),
+                    '1' => __('能', 'cart'),
                 ),
             ],
 
             [
                 'cfg_code' => 'invoice_content',
-                'cfg_name' => __('发票内容', 'article'),
-                'cfg_desc' => __('客户要求开发票时可以选择的内容。例如：办公用品。每一行代表一个选项。', 'article'),
+                'cfg_name' => __('发票内容', 'cart'),
+                'cfg_desc' => __('客户要求开发票时可以选择的内容。例如：办公用品。每一行代表一个选项。', 'cart'),
                 'cfg_range' => '',
             ],
 
             [
                 'cfg_code' => 'invoice_type',
-                'cfg_name' => __('发票类型及税率', 'article'),
-                'cfg_desc' => '',
+                'cfg_name' => __('发票类型及税率', 'cart'),
+                'cfg_desc' => __('税率填写整数值，例如(税率:12，商品金额:35.70)则公式:35.70 * (12 / 100) = 4.28元', 'cart'),
                 'cfg_range' => '',
             ],
 
             [
                 'cfg_code' => 'one_step_buy',
-                'cfg_name' => __('是否一步购物', 'article'),
+                'cfg_name' => __('是否一步购物', 'cart'),
                 'cfg_desc' => '',
                 'cfg_range' => array(
-                    '0' => __('否', 'article'),
-                    '1' => __('是', 'article'),
+                    '0' => __('否', 'cart'),
+                    '1' => __('是', 'cart'),
                 ),
             ],
 
             [
                 'cfg_code' => 'min_goods_amount',
-                'cfg_name' => __('最小购物金额', 'article'),
-                'cfg_desc' => __('达到此购物金额，才能提交订单。', 'article'),
+                'cfg_name' => __('最小购物金额', 'cart'),
+                'cfg_desc' => __('达到此购物金额，才能提交订单。', 'cart'),
                 'cfg_range' => '',
             ],
 
             [
                 'cfg_code' => 'anonymous_buy',
-                'cfg_name' => __('是否允许未登录用户购物', 'article'),
+                'cfg_name' => __('是否允许未登录用户购物', 'cart'),
                 'cfg_desc' => '',
                 'cfg_range' => array(
-                    '0' => __('不允许', 'article'),
-                    '1' => __('允许', 'article'),
+                    '0' => __('不允许', 'cart'),
+                    '1' => __('允许', 'cart'),
                 ),
             ],
 
             [
                 'cfg_code' => 'cart_confirm',
-                'cfg_name' => __('购物车确定提示', 'article'),
-                'cfg_desc' => __('允许您设置用户点击“加入购物车”后是否提示以及随后的动作。', 'article'),
+                'cfg_name' => __('购物车确定提示', 'cart'),
+                'cfg_desc' => __('允许您设置用户点击“加入购物车”后是否提示以及随后的动作。', 'cart'),
                 'cfg_range' => array(
-                    '1' => __('提示用户，点击“确定”进购物车', 'article'),
-                    '2' => __('提示用户，点击“取消”进购物车', 'article'),
-                    '3' => __('直接进入购物车', 'article'),
-                    '4' => __('不提示并停留在当前页面', 'article'),
+                    '1' => __('提示用户，点击“确定”进购物车', 'cart'),
+                    '2' => __('提示用户，点击“取消”进购物车', 'cart'),
+                    '3' => __('直接进入购物车', 'cart'),
+                    '4' => __('不提示并停留在当前页面', 'cart'),
                 ),
             ],
 
             [
                 'cfg_code' => 'stock_dec_time',
-                'cfg_name' => __('减库存的时机', 'article'),
-                'cfg_desc' => '',
+                'cfg_name' => __('减库存的时机', 'cart'),
+                'cfg_desc' => __('例如:选择“下单时”，则表示用户购买商品时，只要下单后，就减少商品的库存', 'cart'),
                 'cfg_range' => array(
-                    '0' => __('发货时', 'article'),
-                    '1' => __('下订单时', 'article'),
+                    '0' => __('发货时', 'cart'),
+                    '1' => __('下订单时', 'cart'),
                 ),
             ],
 
