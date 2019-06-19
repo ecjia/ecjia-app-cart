f3358f89c09759f839580a7b311310dcea27c426
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-06-19 19:22:34 +0800

    封装添加购物车

--- classes/Cart/AddCart.php
+++ classes/Cart/AddCart.php
@@ -35,23 +35,60 @@ class AddCart
     public function checkBuyGoods()
     {
         //1.检查商品是否存在
-
+        $this->checkGoodsExists();
 
         //2.检查商品是否已经下架
-
+        $this->checkGoodsOffSale();
 
         //3.检查商品所属的店铺是否已经下线
-
+        $this->checkGoodsForStoreOffline();
 
         //4.检查商品是否只能作为配件购买，不能单独销售
-
+        $this->checkGoodsNotAloneSale();
 
         //5.检查商品是否有货品
-
+        $this->checkGoodsHasProduct();
 
         //6.检查商品或货品的库存是否满足
+        $this->checkLowStocks();
 
 
+    }
+
+    /**
+     * 1.检查商品是否存在
+     */
+    protected function checkGoodsExists()
+    {
+
+
+
+    }
+
+
+    protected function checkGoodsOffSale()
+    {
+
+    }
+
+
+    protected function checkGoodsForStoreOffline()
+    {
+
+    }
+
+    protected function checkGoodsNotAloneSale()
+    {
+
+    }
+
+    protected function checkGoodsHasProduct()
+    {
+
+    }
+
+    protected function checkLowStocks()
+    {
 
     }
 
